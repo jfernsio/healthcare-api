45186a5b900a87bc87000c87fbc788f747e746c6
Joel Fernandes <150249488+jfernsio@users.noreply.github.com>
2025-01-09 08:25:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'process.env.FRONTEND_URI', 
+  origin: 'https://healthcare-frontend-seven.vercel.app', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
